2fd9d9c2624698799e24d1e8132df1313f546ec1
Nikhil Nanivadekar <nikhil.nanivadekar@gs.com>
2017-03-01 21:39:39 +0000

    Make Pet non-temporal.

--- simple-domain/main/java/example/simple/domain/Pet.java
+++ simple-domain/main/java/example/simple/domain/Pet.java
@@ -1,20 +1,11 @@
 package example.simple.domain;
 
-import java.sql.Timestamp;
-
 public class Pet extends PetAbstract
 {
-    public Pet(Timestamp processingDate
-    )
-    {
-        super(processingDate
-        );
-        // You must not modify this constructor. Mithra calls this internally.
-        // You can call this constructor. You can also add new constructors.
-    }
-
     public Pet()
     {
-        this(com.gs.fw.common.mithra.util.DefaultInfinityTimestamp.getDefaultInfinity());
+        super();
+        // You must not modify this constructor. Mithra calls this internally.
+        // You can call this constructor. You can also add new constructors.
     }
 }
