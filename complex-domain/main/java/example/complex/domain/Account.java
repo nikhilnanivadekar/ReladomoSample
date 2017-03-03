d38d1b458333e65c75487bb8b5d9812893be1239
Nikhil Nanivadekar <nikhil.nanivadekar@gs.com>
2017-03-03 01:55:24 +0000

    Fix codestyle.

--- complex-domain/main/java/example/complex/domain/Account.java
+++ complex-domain/main/java/example/complex/domain/Account.java
@@ -1,27 +1,18 @@
 package example.complex.domain;
+
 import java.sql.Timestamp;
-/********************************************************************************
-* File        : $Source:  $
-* Version     : $Revision:  $
-* Date        : $Date:  $
-* Modified by : $Author:  $
-*******************************************************************************
-*/
+
 public class Account extends AccountAbstract
 {
-	public Account(Timestamp businessDate
-	, Timestamp processingDate
-	)
-	{
-		super(businessDate
-		,processingDate
-		);
-		// You must not modify this constructor. Mithra calls this internally.
-		// You can call this constructor. You can also add new constructors.
-	}
+    public Account(Timestamp businessDate, Timestamp processingDate)
+    {
+        super(businessDate, processingDate);
+        // You must not modify this constructor. Mithra calls this internally.
+        // You can call this constructor. You can also add new constructors.
+    }
 
-	public Account(Timestamp businessDate)
-	{
-		super(businessDate);
-	}
+    public Account(Timestamp businessDate)
+    {
+        super(businessDate);
+    }
 }
